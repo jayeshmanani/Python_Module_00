afebce49b2c0ea4791ffae93fcbc3848959c9074
Jayesh Manani <jmanani@3-C-9.42heilbronn.de>
2025-12-09 20:19:52 +0100

    add: ex7, upd: ex5

--- ex5/ft_count_harvest_iterative.py
+++ ex5/ft_count_harvest_iterative.py
@@ -1,9 +1,8 @@
 def ft_count_harvest_iterative():
     inp = int(input("Days until harvest: "))
     i = 1
-    if (inp):
+    if (inp >= 0):
         while (i <= inp):
             print("Day", i)
             i += 1
         print("Harvest time!")
-    return
