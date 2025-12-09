c341fd37d409f90620b23b4266468ada320776df
Jayesh Manani <jmanani@3-C-9.42heilbronn.de>
2025-12-09 20:04:30 +0100

    add: ex6

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+    garden_name = input("Enter garden name: ")
+    num_of_plants = int(input("Enter number of plants: "))
+    print("Garden:", garden_name)
+    print("Plants:", num_of_plants)
+    print("Status: Growing well!")
