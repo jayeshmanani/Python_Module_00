8f26762bc81afbd1de37c1d27f18e43b45919c06
Jayesh Manani <jmanani@3-C-9.42heilbronn.de>
2025-12-10 15:27:11 +0100

    upd: ex5

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,11 +1,11 @@
-def ft_count_harvest_recursive():
-    days = int(input("Days until harvest: "))
-    count_print(1, days)
-
-
-def count_print(current, total):
-    print(f"Day {current}")
-    if current == total:
-        print("Harvest time!")
+def ft_count_harvest_recursive(days=-1, total=-1):
+    if days == -1:
+        days = int(input("Days until harvest: "))
+        total = days
+    if days == 0:
         return
-    count_print(current + 1, total)
+    else:
+        ft_count_harvest_recursive(days=days-1)
+        print(f"Day {days}")
+    if (days == total):
+        print("Harvest time!")
